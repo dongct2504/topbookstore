20655a88f14ac71fbae89630c35b472bc1d15ff0
keilar000 <dongminhcho89@gmail.com>
2023-10-07 02:00:45 +0700

    link customer and user

--- src/TopBookStore.Infrastructure/Identity/ApplicationDbContext.cs
+++ src/TopBookStore.Infrastructure/Identity/ApplicationDbContext.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TopBookStore.Domain.Entities;
 using TopBookStore.Infrastructure.Identity;
 
 namespace TopBookStore.Infrastructure.Identity;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public DbSet<Customer> Customers { get; set; } = null!;
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
@@ -18,5 +21,11 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        // Map ApplicationUser to Customer
+        builder.Entity<ApplicationUser>()
+            .HasOne<Customer>()
+            .WithOne()
+            .HasForeignKey<ApplicationUser>(a => a.CustomerId);
     }
 }
--- src/TopBookStore.Infrastructure/Identity/ApplicationUser.cs
+++ src/TopBookStore.Infrastructure/Identity/ApplicationUser.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using TopBookStore.Domain.Entities;
 
 namespace TopBookStore.Infrastructure.Identity;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public int CustomerId { get; set; }  // Foreign key property
+    public virtual Customer Customer { get; set; } = null!;  // Navigation property
 }
 
