c4a21bb18380d7c6583e0be7f7f8beb952179a69
keilar000 <dongminhcho89@gmail.com>
2023-10-12 11:38:14 +0700

    fixed name BookCategory

--- dbscripts/InsertTopBookStore.sql
+++ dbscripts/InsertTopBookStore.sql
@@ -1,9 +1,13 @@
 USE TopBookStore;
 
-SELECT * FROM Customers;
-SELECT * FROM Categories;
-SELECT * FROM Books;
-SELECT * FROM Authors;
+SELECT *
+FROM Customers;
+SELECT *
+FROM Categories;
+SELECT *
+FROM Books;
+SELECT *
+FROM Authors;
 
 DELETE FROM BookCategory;
 DELETE FROM Books;
@@ -29,63 +33,76 @@ DBCC CHECKIDENT (Publishers, RESEED, 0)
 DBCC CHECKIDENT (Authors, RESEED, 0)
 
 -- Insert sample Authors
-INSERT INTO Authors (LastName, FirstName, PhoneNumber)
+INSERT INTO Authors
+    (LastName, FirstName, PhoneNumber)
 VALUES
     (N'Nguyễn', N'Văn A', '0901234567'),
     (N'Trần', N'Thị B', '0912345678'),
     (N'Lê', N'Đức C', '0923456789');
 
 -- Insert sample Publishers
-INSERT INTO Publishers (Name)
+INSERT INTO Publishers
+    (Name)
 VALUES
     (N'Nhà Xuất Bản A'),
     (N'Nhà Xuất Bản B'),
     (N'Nhà Xuất Bản C');
 
 -- Insert sample Categories
-INSERT INTO Categories (Name)
+INSERT INTO Categories
+    (Name)
 VALUES
     (N'Tiểu Thuyết'),
     (N'Truyện Ngắn'),
     (N'Sách Kỹ Năng');
 
 -- Insert sample Customers
-INSERT INTO Customers (LastName, FirstName, Email, PhoneNumber, Debt, Street, District, City, Country)
+INSERT INTO Customers
+    (LastName, FirstName, Email, PhoneNumber, Debt, Street, District, City, Country)
 VALUES
     (N'Trần', N'Thị D', 'trand@gmail.com', '0987654321', 0, N'123 Đường A', N'Quận 1', N'TPHCM', N'Việt Nam'),
     (N'Nguyễn', N'Huỳnh E', 'nguyenhe@gmail.com', '0976543210', 0, N'456 Đường B', N'Quận 2', N'TPHCM', N'Việt Nam'),
     (N'Lê', N'Trúc F', 'letruc@gmail.com', '0965432109', 0, N'789 Đường C', N'Quận 3', N'TPHCM', N'Việt Nam');
 
 -- Insert sample Carts
-INSERT INTO Carts (Amount, CustomerId)
+INSERT INTO Carts
+    (Amount, CustomerId)
 VALUES
     (100000, 1),
     (200000, 2),
     (300000, 3);
 
 -- Insert sample Orders
-INSERT INTO Orders (OrderDate, Amount, State, CustomerId)
+INSERT INTO Orders
+    (OrderDate, Amount, State, CustomerId)
 VALUES
     ('2023-10-01', 150000, 'paid', 1),
     ('2023-10-02', 250000, 'sent', 2),
     ('2023-10-03', 350000, 'awaiting', 3);
 
 -- Insert sample Receipts
-INSERT INTO Receipts (Amount, CustomerId)
+INSERT INTO Receipts
+    (Amount, CustomerId)
 VALUES
     (50000, 1),
     (70000, 2),
     (90000, 3);
 
 -- Insert sample Books
-INSERT INTO Books (Title, Description, Isbn13, Inventory, Price, DiscountPercent, NumberOfPages, PublicationDate, AuthorId, PublisherId)
+INSERT INTO Books
+    (Title, Description, Isbn13, Inventory, Price, DiscountPercent,
+    NumberOfPages, PublicationDate, AuthorId, PublisherId)
 VALUES
-    (N'Sách Tiểu Thuyết 1', N'Mô tả sách tiểu thuyết 1', '9781234567890', 10, 150000, 0.1, 200, '2022-01-01', 1, 1),
-    (N'Sách Tiểu Thuyết 2', N'Mô tả sách tiểu thuyết 2', '9780987654321', 5, 200000, 0.2, 250, '2022-05-01', 2, 2),
-    (N'Sách Kỹ Năng 1', N'Mô tả sách kỹ năng 1', '9789876543210', 8, 180000, 0.15, 180, '2022-01-04', 3, 3);
+    (N'Sách Tiểu Thuyết 1', N'Mô tả sách tiểu thuyết 1', '9781234567890', 10, 150000, 0.1,
+        200, '2022-01-01', 1, 1),
+    (N'Sách Tiểu Thuyết 2', N'Mô tả sách tiểu thuyết 2', '9780987654321', 5, 200000, 0.2,
+        250, '2022-05-01', 2, 2),
+    (N'Sách Kỹ Năng 1', N'Mô tả sách kỹ năng 1', '9789876543210', 8, 180000, 0.15,
+        180, '2022-01-04', 3, 3);
 
 -- Insert sample OrderDetails
-INSERT INTO OrderDetails (Quantity, BookId, OrderId)
+INSERT INTO OrderDetails
+    (Quantity, BookId, OrderId)
 VALUES
     (2, 1, 1),
     (1, 2, 1),
@@ -93,15 +110,17 @@ VALUES
     (2, 1, 3);
 
 -- Insert sample CartItems
-INSERT INTO CartItems (Quantity, CartId, BookId)
+INSERT INTO CartItems
+    (Quantity, CartId, BookId)
 VALUES
     (1, 1, 1),
     (2, 1, 2),
     (3, 2, 3),
     (1, 3, 1);
 
--- Insert sample BookCategories
-INSERT INTO BookCategory (BookId, CategoryId)
+-- Insert sample BookCategory
+INSERT INTO BookCategory
+    (BookId, CategoryId)
 VALUES
     (1, 1),
     (2, 1),
--- dbscripts/TopBookStore.sql
+++ dbscripts/TopBookStore.sql
@@ -2,7 +2,7 @@ USE master;
 
 IF DB_ID('TopBookStore') IS NOT NULL
     ALTER DATABASE TopBookStore SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-    DROP DATABASE TopBookStore;
+DROP DATABASE TopBookStore;
 GO
 
 CREATE DATABASE TopBookStore;
@@ -12,129 +12,129 @@ USE TopBookStore;
 
 CREATE TABLE Authors
 (
-  AuthorId INT NOT NULL IDENTITY PRIMARY KEY,
-  FirstName NVARCHAR(60) NOT NULL,
-  LastName NVARCHAR(60) NOT NULL,
-  PhoneNumber VARCHAR(15)
+    AuthorId INT NOT NULL IDENTITY PRIMARY KEY,
+    FirstName NVARCHAR(60) NOT NULL,
+    LastName NVARCHAR(60) NOT NULL,
+    PhoneNumber VARCHAR(15)
 );
 
 CREATE TABLE Publishers
 (
-  PublisherId INT NOT NULL IDENTITY PRIMARY KEY,
-  Name NVARCHAR(80) NOT NULL,
+    PublisherId INT NOT NULL IDENTITY PRIMARY KEY,
+    Name NVARCHAR(80) NOT NULL,
 );
 
 CREATE TABLE Categories
 (
-  CategoryId INT NOT NULL IDENTITY PRIMARY KEY,
-  Name NVARCHAR(80) NOT NULL
+    CategoryId INT NOT NULL IDENTITY PRIMARY KEY,
+    Name NVARCHAR(80) NOT NULL
 );
 
 CREATE TABLE Customers
 (
-  CustomerId INT NOT NULL IDENTITY PRIMARY KEY,
-  FirstName NVARCHAR(80) NOT NULL,
-  LastName NVARCHAR(80) NOT NULL,
-  Email NVARCHAR(70) NOT NULL,
-  PhoneNumber VARCHAR(15) NOT NULL,
-  Debt MONEY NOT NULL DEFAULT 0,
-  Street NVARCHAR(80),
-  District NVARCHAR(50),
-  City NVARCHAR(30),
-  Country NVARCHAR(30)
+    CustomerId INT NOT NULL IDENTITY PRIMARY KEY,
+    FirstName NVARCHAR(80) NOT NULL,
+    LastName NVARCHAR(80) NOT NULL,
+    Email NVARCHAR(70) NOT NULL,
+    PhoneNumber VARCHAR(15) NOT NULL,
+    Debt MONEY NOT NULL DEFAULT 0,
+    Street NVARCHAR(80),
+    District NVARCHAR(50),
+    City NVARCHAR(30),
+    Country NVARCHAR(30)
 );
 
 CREATE TABLE Carts
 (
-  CartId INT NOT NULL IDENTITY PRIMARY KEY,
-  Amount MONEY NOT NULL DEFAULT 0,
-  CustomerId INT NOT NULL,
-  
-  -- When a customer is deleted, all associated cart records should be deleted as well.
-  CONSTRAINT FK_Carts_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
+    CartId INT NOT NULL IDENTITY PRIMARY KEY,
+    Amount MONEY NOT NULL DEFAULT 0,
+    CustomerId INT NOT NULL,
+
+    -- When a customer is deleted, all associated cart records should be deleted as well.
+    CONSTRAINT FK_Carts_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
     ON DELETE CASCADE
 );
 
 CREATE TABLE Orders
 (
-  OrderId INT NOT NULL IDENTITY PRIMARY KEY,
-  OrderDate DATETIME NOT NULL,
-  Amount MONEY NOT NULL,
-  State VARCHAR(30) DEFAULT 'awaiting'
-    CHECK (state = 'awaiting' OR state = 'paid' OR state = 'sent'),
-  CustomerId INT NOT NULL,
+    OrderId INT NOT NULL IDENTITY PRIMARY KEY,
+    OrderDate DATETIME NOT NULL,
+    Amount MONEY NOT NULL,
+    State VARCHAR(30) DEFAULT 'awaiting'
+        CHECK (state = 'awaiting' OR state = 'paid' OR state = 'sent'),
+    CustomerId INT NOT NULL,
 
-  CONSTRAINT FK_Orders_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
+    CONSTRAINT FK_Orders_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
     ON DELETE CASCADE
 );
 
 CREATE TABLE Receipts
 (
-  ReceiptId INT NOT NULL IDENTITY PRIMARY KEY,
-  Amount MONEY NOT NULL DEFAULT 0,
-  CustomerId INT NOT NULL,
+    ReceiptId INT NOT NULL IDENTITY PRIMARY KEY,
+    Amount MONEY NOT NULL DEFAULT 0,
+    CustomerId INT NOT NULL,
 
-  CONSTRAINT FK_Receipts_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
+    CONSTRAINT FK_Receipts_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
     ON DELETE CASCADE
 );
 
 CREATE TABLE Books
 (
-  BookId INT NOT NULL IDENTITY PRIMARY KEY,
-  Title NVARCHAR(80) NOT NULL,
-  Description NVARCHAR(MAX) NOT NULL,
-  Isbn13 VARCHAR(13) NOT NULL,
-  Inventory INT NOT NULL,
-  Price MONEY NOT NULL,
-  DiscountPercent DEC(3, 2) NOT NULL DEFAULT 0,
-  NumberOfPages INT,
-  PublicationDate DATETIME NOT NULL,
-  ImageUrl NVARCHAR(MAX),
-  AuthorId INT NOT NULL,
-  PublisherId INT NOT NULL,
+    BookId INT NOT NULL IDENTITY PRIMARY KEY,
+    Title NVARCHAR(80) NOT NULL,
+    Description NVARCHAR(MAX) NOT NULL,
+    Isbn13 VARCHAR(13) NOT NULL,
+    Inventory INT NOT NULL,
+    Price MONEY NOT NULL,
+    DiscountPercent DEC(3, 2) NOT NULL DEFAULT 0,
+    NumberOfPages INT,
+    PublicationDate DATETIME NOT NULL,
+    ImageUrl NVARCHAR(MAX),
+    AuthorId INT NOT NULL,
+    PublisherId INT NOT NULL,
 
-  -- A Author or Publisher can be deleted only if all books related to those tables
-  -- are deleted
-  CONSTRAINT FK_Books_Authors FOREIGN KEY (AuthorId) REFERENCES Authors(AuthorId)
+    -- A Author or Publisher can be deleted only if all books related to those tables
+    -- are deleted
+    CONSTRAINT FK_Books_Authors FOREIGN KEY (AuthorId) REFERENCES Authors(AuthorId)
     ON DELETE NO ACTION,
-  CONSTRAINT FK_Books_Publishers FOREIGN KEY (PublisherId) REFERENCES Publishers(PublisherId)
+    CONSTRAINT FK_Books_Publishers FOREIGN KEY (PublisherId) REFERENCES Publishers(PublisherId)
     ON DELETE NO ACTION
 );
 
 CREATE TABLE OrderDetails
 (
-  OrderDetailId INT NOT NULL IDENTITY PRIMARY KEY,
-  Quantity INT NOT NULL DEFAULT 0,
-  BookId INT NOT NULL,
-  OrderId INT NOT NULL,
+    OrderDetailId INT NOT NULL IDENTITY PRIMARY KEY,
+    Quantity INT NOT NULL DEFAULT 0,
+    BookId INT NOT NULL,
+    OrderId INT NOT NULL,
 
-  CONSTRAINT FK_OrderDetails_Books FOREIGN KEY (BookId) REFERENCES Books(BookId)
+    CONSTRAINT FK_OrderDetails_Books FOREIGN KEY (BookId) REFERENCES Books(BookId)
     ON DELETE CASCADE,
-  CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (OrderId) REFERENCES Orders(OrderId)
+    CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (OrderId) REFERENCES Orders(OrderId)
     ON DELETE CASCADE
 );
 
 CREATE TABLE CartItems
 (
-  CartItemId INT NOT NULL IDENTITY PRIMARY KEY,
-  Quantity INT NOT NULL,
-  CartId INT NOT NULL,
-  BookId INT NOT NULL,
+    CartItemId INT NOT NULL IDENTITY PRIMARY KEY,
+    Quantity INT NOT NULL,
+    CartId INT NOT NULL,
+    BookId INT NOT NULL,
 
-  CONSTRAINT FK_CartItems_Carts FOREIGN KEY (CartId) REFERENCES Carts(CartId)
+    CONSTRAINT FK_CartItems_Carts FOREIGN KEY (CartId) REFERENCES Carts(CartId)
     ON DELETE CASCADE,
-  CONSTRAINT FK_CartItems_Books FOREIGN KEY (BookId) REFERENCES Books(BookId)
+    CONSTRAINT FK_CartItems_Books FOREIGN KEY (BookId) REFERENCES Books(BookId)
     ON DELETE CASCADE
 );
 
-CREATE TABLE BookCategories
+CREATE TABLE BookCategory
 (
-  BookId INT NOT NULL,
-  CategoryId INT NOT NULL,
-  PRIMARY KEY (BookId, CategoryId),
+    BookId INT NOT NULL,
+    CategoryId INT NOT NULL,
+    PRIMARY KEY (BookId, CategoryId),
 
-  CONSTRAINT FK_BookCategories_Books FOREIGN KEY (BookId) REFERENCES Books(BookId)
+    CONSTRAINT FK_BookCategory_Books FOREIGN KEY (BookId) REFERENCES Books(BookId)
     ON DELETE CASCADE,
-  CONSTRAINT FK_BookCategories_Categories FOREIGN KEY (CategoryId) REFERENCES Categories(CategoryId)
+    CONSTRAINT FK_BookCategory_Categories FOREIGN KEY (CategoryId) REFERENCES Categories(CategoryId)
     ON DELETE CASCADE
 );
